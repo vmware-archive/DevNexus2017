7543261d355d092a994235e6edbe66c337f262c0
Mike Wright <mwright@gopivotal.com>
2017-02-16 11:10:46 -0500

    Update build-docker-service.sh

--- 4-Advanced Topics/labs/fortune-teller-demo/build-docker-service.sh
+++ 4-Advanced Topics/labs/fortune-teller-demo/build-docker-service.sh
@@ -3,6 +3,7 @@ echo "HINT 2: Did you update pom.xml, to point to your docker hub account?"
 echo "HINT 3: Did you issue 'docker login'?"
 echo "HINT 4: Are you running in a shell setup by Docker Tools?"
 echo "Idea: Man, wouldn't it be easier if I didn't need all this stuff??"
+echo "    ... just saying"
 
 cd fortune-gapowerball
 mvn package docker:build -DpushImage
