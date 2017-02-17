408061824ba016357ddc5db11a7ec13c4d66eda5
Mike Wright <mwright@pivotal.io>
2017-02-17 09:19:38 -0500

    fresh build of .NET service

--- 4-Advanced Topics/Demo/fortune-teller-demo/build-service.sh
+++ 4-Advanced Topics/Demo/fortune-teller-demo/build-service.sh
@@ -1,4 +1,4 @@
 cd Fortune-Teller-Service
 dotnet restore --configfile nuget.config
-dotnet publish --output publish --configuration Release --framework netcoreapp1.0 --runtime ubuntu.14.04-x64
+dotnet publish --output publish --configuration Release --framework netcoreapp1.1 --runtime ubuntu.14.04-x64
 cd ..
