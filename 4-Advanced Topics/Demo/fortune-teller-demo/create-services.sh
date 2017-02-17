ec2e320d7307c6c1c0653aa9ceeff1fb02621749
Mike Wright <mwright@pivotal.io>
2017-02-17 09:08:39 -0500

    clean up pom

--- 4-Advanced Topics/Demo/fortune-teller-demo/create-services.sh
+++ 4-Advanced Topics/Demo/fortune-teller-demo/create-services.sh
@@ -1,3 +1,3 @@
-cf create-service p-config-server standard config-server-poly -c config-server-orig.json
+cf create-service p-config-server standard config-server-poly -c config-server.json
 cf create-service p-service-registry standard discovery-service-poly
 cf create-service p-circuit-breaker-dashboard standard circuit-breaker-poly
