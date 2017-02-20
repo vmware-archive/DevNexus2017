d8864abbfa7adcf755d3e9849f1a1b1272cd5f71
Mike Wright <mwright@pivotal.io>
2017-02-19 19:53:57 -0500

    point config to a different repo

--- 4-Advanced Topics/Demo/fortune-teller-demo/push-docker-powerball.sh
+++ 4-Advanced Topics/Demo/fortune-teller-demo/push-docker-powerball.sh
@@ -1,5 +1,7 @@
 cf push powerball-docker -o mwrightpivotal/gapowerball -m 2G --no-start
 cf set-env powerball-docker CF_TARGET https://api.run.haas-73.pez.pivotal.io
+cf set-env powerball-docker MANAGEMENT_CLOUDFOUNDRY_SKIP-SSL-VALIDATION true
+cf set-env powerball-docker SECURITY_BASIC_ENABLED false
 cf bind-service powerball-docker config-server-poly
 cf bind-service powerball-docker discovery-service-poly
 cf start powerball-docker 
